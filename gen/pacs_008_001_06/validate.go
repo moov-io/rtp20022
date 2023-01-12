3a27b6b91b46d3e5df013e7e0788654c2ba4138f
Adam Shannon <adamkshannon@gmail.com>
2023-01-12 16:43:56 -0600

    gen/pacs_008_001_06: delete extra files

--- gen/pacs_008_001_06/validate.go
+++
@@ -1,9 +0,0 @@
-package pacs_008_001_06
-
-func (d Document) Validate() error {
-	return d.FIToFICstmrCdtTrf.Validate()
-}
-
-func (c FIToFICustomerCreditTransferV06) Validate() error {
-	return nil // TODO(adam):
-}
