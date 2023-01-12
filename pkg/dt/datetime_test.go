f5dd84e7fa178638ea3f26459ea457c0a42c1dda
Adam Shannon <adamkshannon@gmail.com>
2023-01-12 16:41:28 -0600

    feat: create separate dt package for RTP compliant date/times

---
+++ pkg/dt/datetime_test.go
@@ -0,0 +1,45 @@
+package dt
+
+import (
+	"encoding/xml"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestISODateFormat(t *testing.T) {
+	loc, _ := time.LoadLocation("America/New_York")
+	when := time.Date(2019, time.March, 21, 0, 0, 0, 0, loc)
+
+	out, err := ISODate(when).MarshalText()
+	require.NoError(t, err)
+	require.Equal(t, "2019-03-21", string(out))
+
+	out, err = xml.Marshal(ISODate(when))
+	require.NoError(t, err)
+	require.Equal(t, "<ISODate>2019-03-21</ISODate>", string(out))
+
+	var read ISODate
+	err = xml.Unmarshal([]byte("<ISODate>2019-03-21</ISODate>"), &read)
+	require.NoError(t, err)
+	require.True(t, when.Equal(time.Time(read)))
+}
+
+func TestISODateTimeFormat(t *testing.T) {
+	loc, _ := time.LoadLocation("America/New_York")
+	when := time.Date(2019, time.March, 21, 10, 36, 19, 0, loc)
+
+	out, err := ISODateTime(when).MarshalText()
+	require.NoError(t, err)
+	require.Equal(t, "2019-03-21T10:36:19", string(out))
+
+	out, err = xml.Marshal(ISODateTime(when))
+	require.NoError(t, err)
+	require.Equal(t, "<ISODateTime>2019-03-21T10:36:19</ISODateTime>", string(out))
+
+	var read ISODateTime
+	err = xml.Unmarshal([]byte("<ISODateTime>2019-03-21T10:36:19</ISODateTime>"), &read)
+	require.NoError(t, err)
+	require.True(t, when.Equal(time.Time(read)))
+}
