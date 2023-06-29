7e6cc1fd8aa8185216b871f8c605d1d2cb0d69cb
Jason Bornstein <131717043+jasonbornsteinMOOV@users.noreply.github.com>
2023-06-29 17:00:19 -0400

    update generator

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -13,4 +13,19 @@ moovio_xsd2go convert \
    github.com/moov-io/rtp20022 \
    gen \
    --template-package=internal/templates/rtp20022 \
-   --template-name=messages.tmpl
+   --template-name=messages.tmpl \
+   --xmlns-override="urn:iso:std:iso:20022:tech:xsd:head.001.001.01=head_001_001_01" \
+   --xmlns-override="urn:iso:std:iso:20022:tech:xsd:admi.002.001.01=admi_002_001_01" \
+   --xmlns-override="urn:iso:std:iso:20022:tech:xsd:admi.004.001.02=admi_004_001_02" \
+   --xmlns-override="urn:iso:std:iso:20022:tech:xsd:admn.001.001.01=admn_001_001_01" \
+   --xmlns-override="urn:iso:std:iso:20022:tech:xsd:pain.013.001.07=pain_013_001_07" \
+   --xmlns-override="urn:iso:std:iso:20022:tech:xsd:pacs.002.001.10=pacs_002_001_10" \
+   --xmlns-override="urn:iso:std:iso:20022:tech:xsd:camt.035.001.05=camt_035_001_05" \
+   --xmlns-override="urn:iso:std:iso:20022:tech:xsd:camt.056.001.08=camt_056_001_08" \
+   --xmlns-override="urn:iso:std:iso:20022:tech:xsd:camt.029.001.09=camt_029_001_09" \
+   --xmlns-override="urn:iso:std:iso:20022:tech:xsd:admn.002.001.01=admn_002_001_01" \
+   --xmlns-override="urn:iso:std:iso:20022:tech:xsd:admn.005.001.01=admn_005_001_01" \
+   --xmlns-override="urn:iso:std:iso:20022:tech:xsd:admn.006.001.01=admn_006_001_01" \
+   --xmlns-override="urn:iso:std:iso:20022:tech:xsd:admn.004.001.01=admn_004_001_01" \
+   --xmlns-override="urn:iso:std:iso:20022:tech:xsd:admn.003.001.01=admn_003_001_01" \
+   --xmlns-override="urn:iso:std:iso:20022:tech:xsd:pacs.008.001.08=pacs_008_001_08"
